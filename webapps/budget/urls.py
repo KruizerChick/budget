4711580bfa2c7e9c9bcfbd87cd8cd497a60a842e
KruizerChick <KruizerChick@gmail.com>
2018-05-11 15:45:57 -0500

    Add models, views, and templatesk for new projects.

--- webapps/budget/urls.py
+++ webapps/budget/urls.py
@@ -7,6 +7,7 @@ app_name = 'budget'
 
 urlpatterns = [
     path('', views.project_list, name='list'),
+    path('add', views.ProjectCreateView.as_view(), name='add'),
     path('<slug:project_slug>', views.project_detail, name='detail'),
 
 ]
